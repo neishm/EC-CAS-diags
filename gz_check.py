cad33968056741c374ebe435b47e18976ba48944
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-10 20:05:54 +0000

    Update gz_check to print the lowest 8 model heights

--- gz_check.py
+++ gz_check.py
@@ -4,8 +4,7 @@ from gaw_obs import obs_locations, data as obs
 from model import gz
 
 # Take GZ at the 'surface' level of the model, and assume it doesn't vary with time.
-gz2 = gz(i_time=0).squeeze(eta=0.995)*10
-gz = gz(i_time=0).squeeze(eta=1)*10
+gz = gz(i_time=0).squeeze()*10
 
 print "%40s%30s%10s%10s%15s%15s"%('Location', 'Country', 'Lat', 'Lon', 'obs height', 'model height')
 print '-'*120
@@ -13,5 +12,7 @@ print '-'*120
 for location in sorted(obs_locations.keys()):
   lat, lon, country = obs_locations[location]
   if lon < 0: lon += 360
-  print "%40s%30s%10.2f%10.2f%15d%15d"%(location,country,lat,lon,obs[location].atts['altitude'], gz.squeeze(lat=lat,lon=lon).get()), gz2.squeeze(lat=lat,lon=lon).get()
+  gz_profile = gz.squeeze(lat=lat,lon=lon)[-1:-9:-1]
+  gz_profile = ', '.join('%4d'%int(g) for g in gz_profile)
+  print "%40s%30s%10.2f%10.2f%15d     "%(location,country,lat,lon,obs[location].atts['altitude']), gz_profile
 
