2dbc7f18ef38329992bdfe8377b14b48295e6d5b
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-14 13:39:48 +0000

    Update CarbonTracker interface
    Define a ct_open method to handle the proper wrapping of axes, etc.

--- carbontracker.py
+++ carbontracker.py
@@ -1,8 +1,5 @@
 # Interface for CarbonTracker data
 
-from pygeode.formats import netcdf as nc
-from pygeode.formats.multifile import openall, open_multi
-
 def ct_file2date(filename):
   from re import search
   date = search("(?P<year>\d{4})-?(?P<month>\d{2})-?(?P<day>\d{2})\.nc$", filename).groupdict()
@@ -11,9 +8,22 @@ def ct_file2date(filename):
   date['day']   = int(date['day'])
   return date
 
-molefractions = open_multi(files="/wrk1/EC-CAS/CarbonTracker/molefractions/CT2010.molefrac_glb3x2_????-??-??.nc", format=nc, file2date=ct_file2date)
+def ct_open (files):
+  from pygeode.formats.multifile import open_multi
+  from pygeode.formats import netcdf as nc
+  from common import fix_timeaxis
+  data = open_multi(files=files, format=nc, file2date=ct_file2date)
+  data = data.rename_axes(date='time')
+  data = fix_timeaxis(data)
+  return data
 
-molefractions_interface = open_multi(files="/wrk1/EC-CAS/CarbonTracker/molefractions_interface/molefrac_glb3x2_????-??-??.nc", format=nc, file2date=ct_file2date)
+molefractions = ct_open("/wrk1/EC-CAS/CarbonTracker/molefractions/CT2010.molefrac_glb3x2_????-??-??.nc")
 
-fluxes = open_multi(files="/wrk1/EC-CAS/CarbonTracker/fluxes/CT2010.flux1x1.????????.nc", format=nc, file2date=ct_file2date)
+molefractions_interface = ct_open("/wrk1/EC-CAS/CarbonTracker/molefractions_interface/molefrac_glb3x2_????-??-??.nc")
+
+fluxes = ct_open("/wrk1/EC-CAS/CarbonTracker/fluxes/CT2010.flux1x1.????????.nc")
+
+# Find the total CO2 (sum of components)
+co2 = molefractions.bg + molefractions.ff + molefractions.bio + molefractions.ocean + molefractions.fires
+
 
