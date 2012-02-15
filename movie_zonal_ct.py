83b3d5f7ffe7f9cd48408095acb4fa872ef00548
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-15 15:46:39 +0000

    cumulative updates for zonal GEM/CT movies

--- movie_zonal_ct.py
+++ movie_zonal_ct.py
@@ -4,21 +4,28 @@ from carbontracker import molefractions
 
 from pygeode.climat import dailymean
 
+import numpy as np
+
 # Interpolate to geopotential height
 from pygeode.interp import interpolate
 from model import dynamics
 from pygeode.axis import Height
 height = Height(range(68))
-ct_co2 = interpolate(molefractions.bio, inaxis='level', outaxis=height, inx = molefractions.gph/1000)
+field = 'ff'
+ct_co2 = interpolate(molefractions[field], inaxis='level', outaxis=height, inx = molefractions.gph/1000)
 ct_co2 = ct_co2.mean('lon')
 ct_co2 = ct_co2.transpose(0,2,1)
 ct_co2 = dailymean(ct_co2)
 
-from model import cla_zonal
-gem_co2 = cla_zonal - 100
+from model import cff_zonal as gem_co2
+#gem_co2 = gem_co2 - 100  # Biogenic offset
+#gem_co2 = gem_co2 - 100  # Ocean offset
 
 from movie_zonal import create_images
-contours = range(376,402+1,1) # total CO2 range
-contours = range(-30,4) # biogenic range
+#contours = range(376,402+1,1) # total CO2 range
+#contours = range(-30,4) # biogenic range
+#contours = np.arange(-12,-2+0.2,0.5) # ocean range
+#contours = np.arange(4,12+0.2,0.2) # fires range
+contours = np.arange(10,50) # fossil fuels range
 create_images (gem_co2, ct_co2, contours = contours, refname='CarbonTracker')
 
