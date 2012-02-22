b26d7af5e01a660a90e6a2983d5b8d48ba75f968
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-22 22:03:12 +0000

    Generalize the interface to the model RPN files
    Provide a method which loads data from a given directory, returns PyGeode datasets.
    This can either be used directly, or fed into netcdf.save to create intermediate
    netcdf files.

---
+++ model_stuff.py
@@ -0,0 +1,74 @@
+
+def rpnopen (filename):
+  from pygeode.formats import rpn
+  f = rpn.open(filename)
+#  f = f.squeeze(rpn.F)
+  f = f.squeeze()
+  return f
+
+def rpnopen_sfconly (filename):
+  f = rpnopen(filename)
+  f = f(eta=1).squeeze()
+  return f
+
+def rpnopen_eta932 (filename):
+  f = rpnopen(filename)
+  f = f(eta=0.932)
+  return f
+
+# Extract a date from a GEM model filename
+def file2date (filename):
+  from re import search
+  from datetime import datetime, timedelta
+  out = search('[dkp][m](?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})(?P<hour>\d{2})_(?P<offset>\d+)(?P<units>[m])$', filename).groupdict()
+  units = out.pop('units')
+  out = dict([k,int(v)] for k,v in out.items())
+  offset = out.pop('offset')
+
+  # Convert to datetime object for manipulating the forecast time
+  out = datetime(**out)
+  if units == 'm':
+    out += timedelta (minutes=offset)
+  elif units == 'h':
+    out += timedelta (hours=offset)
+  else: raise Exception
+
+  # Convert back to dictionary
+  out = dict(year=out.year, month=out.month, day=out.day, hour=out.hour, minute=out.minute)
+  return out
+
+
+# Extract a date from an analysis file
+def file2date_anlm (filename):
+  from re import search
+  from datetime import datetime, timedelta
+  out = search('anlm(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})(?P<hour>\d{2})_000$', filename).groupdict()
+  out = dict([k,int(v)] for k,v in out.items())
+  return out
+
+
+def open (dirname):
+  from pygeode.formats.multifile import open_multi
+
+  data = dict()
+
+  data['dm'] = dm = open_multi (dirname+"/dm*_1440m", opener=rpnopen, file2date=file2date)
+  data['dm_zonalmean_eta'] = dm.mean('lon')
+  data['km_sfc'] = open_multi (dirname+"/km*", opener=rpnopen_sfconly, file2date=file2date)
+  data['eta932'] = open_multi (dirname+"/km*", opener=rpnopen_eta932, file2date=file2date)
+  data['km_daily'] = km_daily = open_multi (dirname+"/km*_1440m", opener=rpnopen, file2date=file2date)
+  data['km_zonalmean_eta'] = km_daily.mean('lon')
+
+  # Convert zonal mean data (on height)
+  from pygeode.interp import interpolate
+  from pygeode.axis import Height
+  from pygeode.dataset import Dataset
+  height = Height(range(68), name='height')
+  varlist = [var for var in km_daily if var.hasaxis('eta')]
+  varlist = [interpolate(var, inaxis='eta', outaxis=height, inx=dm.GZ*10/1000) for var in varlist]
+  varlist = [var.mean('lon') for var in varlist]
+  varlist = [var.transpose(0,2,1) for var in varlist]
+  data['km_zonalmean_gph'] = Dataset(varlist)
+
+  return data
+
