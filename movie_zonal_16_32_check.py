a5408e64f875b0b0ae4cdb7664e24f7f5ae90d8c
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-23 14:13:16 +0000

    zonal movie compairing CO2 from 16-bit and 32-bit runs

---
+++ movie_zonal_16_32_check.py
@@ -0,0 +1,10 @@
+from movie_zonal import create_images
+from model_stuff import open
+from common import convert
+
+co2_16 = open("/wrk1/neish/validation_run_16bit")['km_zonalmean_eta']['CO2'] * convert
+co2_32 = open("/wrk1/neish/validation_run_32bit")['km_zonalmean_eta']['CO2'] * convert
+
+contours = range(376,402+1,1) # total CO2 range
+
+create_images (co2_16, co2_32, title1='CO2 (16-bit)', title2='CO2 (32-bit)', contours=contours)
