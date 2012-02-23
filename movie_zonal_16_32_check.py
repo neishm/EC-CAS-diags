ff2bacd19e3ae25bce3a57d68e8b73b90b254936
Mike Neish <Michael.Neish@ec.gc.ca>
2012-02-23 17:02:50 +0000

    update 16-bit/32-bit zonal movie to use cached netcdf files

--- movie_zonal_16_32_check.py
+++ movie_zonal_16_32_check.py
@@ -2,9 +2,11 @@ from movie_zonal import create_images
 from model_stuff import open
 from common import convert
 
-co2_16 = open("/wrk1/neish/validation_run_16bit")['co2_zonalmean_eta']['CO2'] * convert
-co2_32 = open("/wrk1/neish/validation_run_32bit")['co2_zonalmean_eta']['CO2'] * convert
+co2_16 = open("/wrk1/neish/validation_run_16bit","/wrk1/neish/nc_cache/validation_run_16bit")
+co2_16 = co2_16['co2_zonalmean_eta']['CO2'] * convert
+co2_32 = open("/wrk1/neish/validation_run_32bit","/wrk1/neish/nc_cache/validation_run_32bit")
+co2_32 = co2_32['co2_zonalmean_eta']['CO2'] * convert
 
 contours = range(376,402+1,1) # total CO2 range
 
-create_images (co2_16, co2_32, title1='CO2 (16-bit)', title2='CO2 (32-bit)', contours=contours)
+#create_images (co2_16, co2_32, title1='CO2 (16-bit)', title2='CO2 (32-bit)', contours=contours)
