4f3a34eb2db8bdf3b7163e7b709f1a430ee2c6c3
Mike Neish <Michael.Neish@ec.gc.ca>
2012-03-06 16:15:38 +0000

    add CarbonTracker panel to mc_glitch script

--- mc_glitch.py
+++ mc_glitch.py
@@ -1,14 +1,23 @@
 # Compare CO2 field with/without mass conservation on vertical diffusion
 
+import numpy as np
 from model_stuff import my_data
 from movie_zonal import create_images
 from common import convert_CO2
+from carbontracker import co2 as ct_co2, ct_zonal, molefractions
+ct_co2 = ct_zonal(ct_co2)
+#ct_co2 = ct_zonal(molefractions['bio'])
 
-data = my_data("validation_run_32bit")['km_zonalmean_eta'](year=2009,month=(1,6))
-data6b = my_data("cycletest6b")['km_zonalmean_eta'](year=2009,month=(1,6))
+data = my_data("validation_run_32bit")['km_zonalmean_gph']
+data6b = my_data("cycletest6b")['km_zonalmean_gph']
 
-mc = data['CONS'] * convert_CO2
-nomc = data6b['CONS'] * convert_CO2
+#mc = data['CONS'] * convert_CO2
+#nomc = data6b['CONS'] * convert_CO2
+mc = data['CO2'] * convert_CO2
+nomc = data6b['CO2'] * convert_CO2
+#mc = data['CLA'] * convert_CO2 - 100
+#nomc = data6b['CLA'] * convert_CO2 - 100
 
-create_images(mc, nomc, title1='CONS (mass conservation)', title2='CONS (no mass cons.)')
-#create_images(mc-nomc, title1='CONS conserv/non-conserv difference')
+#create_images(mc, nomc, title1='CONS (mass conservation)', title2='CONS (no mass cons.)', contours = np.arange(374.66,375.14+0.02,0.02), palette='Paired')
+create_images(mc, nomc, ct_co2, title1='CO2 (mass conservation)', title2='CO2 (no mass cons.)', title3='CarbonTracker CO2')
+#create_images(mc, nomc, ct_co2, title1='CLA (mass conservation)', title2='CLA (no mass cons.)', title3='CarbonTracker biog. CO2')
