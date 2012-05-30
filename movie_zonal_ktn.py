5b4cca783cd87db18fe856999106f2b130113bd7
Mike Neish <Michael.Neish@ec.gc.ca>
2012-05-30 13:41:55 +0000

    Add 'KT' to the KTN zonal mean movie.

--- movie_zonal_ktn.py
+++ movie_zonal_ktn.py
@@ -2,5 +2,6 @@ from model_stuff import my_data
 from movie_zonal import create_images
 
 KTN = my_data("cycletest5b")['km_zonalmean_eta']['KTN']
+KT  = my_data("cycletest5b")['pm_zonalmean_eta']['KT']
 
-create_images(KTN, title1="KT (chem version)")
+create_images(KTN, KT, title1="KT (chem version)", title2="KT (physics version)")
