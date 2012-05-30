da202db8cbf6c772043c02d02b30d5da1bcfab07
Mike Neish <Michael.Neish@ec.gc.ca>
2012-05-30 13:51:45 +0000

    Remove experiment-specific scripts from the master branch.
    If these are needed in the future, they should be pulled into an experiment branch.

--- movie_zonal_ktn.py
+++
@@ -1,7 +0,0 @@
-from model_stuff import my_data
-from movie_zonal import create_images
-
-KTN = my_data("cycletest5b")['km_zonalmean_eta']['KTN']
-KT  = my_data("cycletest5b")['pm_zonalmean_eta']['KT']
-
-create_images(KTN, KT, title1="KT (chem version)", title2="KT (physics version)")
--- movie_zonal_p5_p7.py
+++
@@ -1,8 +0,0 @@
-from movie_zonal import create_images
-from model_stuff import my_data
-from common import convert_CO2
-
-co2_p5 = my_data("validation_run_32bit")['km_zonalmean_eta']['CO2'] * convert_CO2
-co2_p7 = my_data("cycletest6")['km_zonalmean_eta']['CO2'] * convert_CO2
-
-create_images (co2_p5, co2_p7, title1='CO2 (P5 run)', title2='CO2 (P7 run)')
