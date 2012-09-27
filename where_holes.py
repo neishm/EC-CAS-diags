bcad34117593cfab38e27989af09d9e91b419c7a
Mike Neish <Michael.Neish@ec.gc.ca>
2012-09-27 19:36:53 +0000

    Wrap where_holes so it can be called from a diagnostic suite.

--- where_holes.py
+++ where_holes.py
@@ -1,13 +1,21 @@
-from interfaces import experiment, experiment_name
-from common import convert_CO2
-from pygeode.formats import netcdf
+def where_holes (outdir):
+  from interfaces import experiment, experiment_name
+  from common import convert_CO2
+  from pygeode.formats import netcdf
+  from os.path import exists
 
-co2 = experiment['sfc']['CO2'] * convert_CO2
+  co2 = experiment['sfc']['CO2'] * convert_CO2
 
-holes = (co2 < 100)
+  holes = (co2 < 100)
 
-count = (holes * 1).sum('time')
-count.name = "hole_count"
+  count = (holes * 1).sum('time')
+  count.name = "hole_count"
 
-netcdf.save("%s_hole_count.nc"%experiment_name, count)
+  outfile = outdir+"/%s_hole_count.nc"%experiment_name
+  if not exists(outfile):
+    netcdf.save(outfile, count)
 
+if __name__ == '__main__':
+  from interfaces import experiment_name
+  outdir = experiment_name
+  where_holes (outdir=outdir)
