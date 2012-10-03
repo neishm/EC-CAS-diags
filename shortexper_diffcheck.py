115f3940b2089f678f3a4770d2dec75200b56277
Mike Neish <Michael.Neish@ec.gc.ca>
2012-10-03 18:27:34 +0000

    Use the same contour range for experiment & control in shortexper_diffcheck.

--- shortexper_diffcheck.py
+++ shortexper_diffcheck.py
@@ -4,6 +4,7 @@ def shortexper_diffcheck(experiment, control, location, outdir):
   from common import convert_CO2
   from os.path import exists
   from pygeode.plot import plotvar
+  from contouring import get_range, get_contours
   from matplotlib import pyplot as pl
 
   lat, lon, country = obs_locations[location]
@@ -13,13 +14,49 @@ def shortexper_diffcheck(experiment, control, location, outdir):
   time = experiment.dm.time.values
   co2_obs = co2_obs(time=(min(time),max(time)))
 
-  fig = pl.figure(figsize=(15,15))
+  if control is not None:
+    fig = pl.figure(figsize=(15,15))
+    n = 2
+  else:
+    fig = pl.figure(figsize=(8,15))
+    n = 1
 
+  ktn_min = float('inf')
+  ktn_max = float('-inf')
+  co2_min = float('inf')
+  co2_max = float('-inf')
+  co2_sfc_min = co2_obs.min()
+  co2_sfc_max = co2_obs.max()
+
+  # Get the data, and compute the global ranges
   for i,dataset in enumerate([experiment,control]):
+
+    if dataset is None: continue
+
+    ktn = dataset.get_data('km',location,'KTN')
+    mn, mx = get_range(ktn)
+    ktn_min = min(ktn_min, mn)
+    ktn_max = max(ktn_max, mx)
+
+    co2 = dataset.get_data('dm',location,'CO2') * convert_CO2
+    mn, mx = get_range(co2)
+    co2_min = min(co2_min, mn)
+    co2_max = max(co2_max, mx)
+
+    co2_sfc_min = min(co2_sfc_min, co2(eta=1).min())
+    co2_sfc_max = max(co2_sfc_max, co2(eta=1).max())
+
+  # Do the plots
+  for i,dataset in enumerate([experiment,control]):
+
+    if dataset is None: continue
 
     ktn = dataset.get_data('km',location,'KTN')
     co2 = dataset.get_data('dm',location,'CO2') * convert_CO2
     co2.name = 'CO2'
+
+    # Put the variables on a height coordinate
+    # TODO: proper vertical interpolation
     gz = dataset.get_data('dm',location,'GZ')(i_time=0).squeeze()
     height = Height(gz.get() * 10)  # decametres to metres
     ktn = ktn.replace_axes(eta=height)
@@ -28,17 +65,19 @@ def shortexper_diffcheck(experiment, control, location, outdir):
     # Adjust pbl to use the same height units for plotting.
     pbl *= Height.plotatts.get('scalefactor',1)
 
-    axis = pl.subplot(3,2,0*2+i+1)
-    plotvar(ktn(z=(0,10000)), ax=axis, title='%s KTN (%s)'%(location,dataset.name))
+    axis = pl.subplot(3,n,0*n+i+1)
+    plotvar(ktn(z=(0,10000)), ax=axis, title='%s KTN (%s)'%(location,dataset.name), clevs=get_contours(ktn_min,ktn_max))
     plotvar(pbl, color='white', ax=axis, hold=True)
 
-    axis = pl.subplot(3,2,1*2+i+1)
-    plotvar(co2(z=(0,10000)), ax=axis, title='%s CO2 (%s)'%(location,dataset.name))
+    axis = pl.subplot(3,n,1*n+i+1)
+    plotvar(co2(z=(0,10000)), ax=axis, title='%s CO2 (%s)'%(location,dataset.name), clevs=get_contours(co2_min,co2_max))
     plotvar(pbl, color='white', ax=axis, hold=True)
 
-    axis = pl.subplot(3,2,2*2+i+1)
+    axis = pl.subplot(3,n,2*n+i+1)
     plotvar(co2(z=0), color='blue', ax=axis, title='%s CO2 (%s)'%(location,dataset.name))
     plotvar(co2_obs, color='green', ax=axis, hold=True)
+    axis.set_ylim([co2_sfc_min,co2_sfc_max])
+    axis.legend ([dataset.title, 'Obs'])
 
   outfile = outdir+"/%s_%s_diffcheck.png"%(experiment.name,location)
   if not exists(outfile):
