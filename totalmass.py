f935b692194100968ce4bae3dec26ad986e0077d
Mike Neish <Michael.Neish@ec.gc.ca>
2012-10-17 15:36:07 +0000

    Add background CO2 to total mass plots.

--- totalmass.py
+++ totalmass.py
@@ -8,18 +8,23 @@ def totalmass (experiment, control, outdir):
   if exists(outfile): return
 
   exper_mass = experiment.get_data('dm', 'totalmass', 'CO2')
+  exper_bgmass = experiment.get_data('dm', 'totalmass', 'CO2B')
   if control is not None:
     control_mass = control.get_data('dm', 'totalmass', 'CO2')
+    control_bgmass = control.get_data('dm', 'totalmass', 'CO2B')
   ct_mass = ct['totalmass']['co2']
 
   fig = pl.figure(figsize=(15,12))
   ax = pl.subplot(111)
-  titles = [experiment.title, 'CarbonTracker']
+  titles = [experiment.title, 'background', 'CarbonTracker']
   plotvar (exper_mass, color='blue', ax=ax, title="Total mass CO2 (Pg C)")
+  plotvar (exper_bgmass, color='blue', ax=ax, linestyle=':', hold=True)
   plotvar (ct_mass, color='green', ax=ax, hold=True)
   if control is not None:
     plotvar (control_mass, color='red', ax=ax, hold=True)
+    plotvar (control_bgmass, color='red', ax=ax, linestyle=':', hold=True)
     titles.append(control.title)
+    titles.append("control bg")
   pl.legend(titles)
 
   fig.savefig(outfile)
