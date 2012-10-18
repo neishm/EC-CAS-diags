0349a1c70a9f343e3d77aa6dead2409ed3c3464b
Mike Neish <Michael.Neish@ec.gc.ca>
2012-10-18 17:55:42 +0000

    Refactor total mass diagnostic, to make it easier to extend to more fields.

--- totalmass.py
+++ totalmass.py
@@ -1,31 +1,39 @@
-def totalmass (experiment, control, outdir):
+def doplot (outfile, title, fields, colors, styles, labels):
   from pygeode.plot import plotvar
   import matplotlib.pyplot as pl
-  from carbontracker import data as ct
-  from os.path import exists
-
-  outfile = outdir + "/%s_totalmass_CO2.png"%experiment.name
-  if exists(outfile): return
-
-  exper_mass = experiment.get_data('dm', 'totalmass', 'CO2')
-  exper_bgmass = experiment.get_data('dm', 'totalmass', 'CO2B')
-  if control is not None:
-    control_mass = control.get_data('dm', 'totalmass', 'CO2')
-    control_bgmass = control.get_data('dm', 'totalmass', 'CO2B')
-  ct_mass = ct['totalmass']['co2']
 
   fig = pl.figure(figsize=(15,12))
   ax = pl.subplot(111)
-  titles = [experiment.title, 'background', 'CarbonTracker']
-  plotvar (exper_mass, color='blue', ax=ax, title="Total mass CO2 (Pg C)")
-  plotvar (exper_bgmass, color='blue', ax=ax, linestyle=':', hold=True)
-  plotvar (ct_mass, color='green', ax=ax, hold=True)
-  if control is not None:
-    plotvar (control_mass, color='red', ax=ax, hold=True)
-    plotvar (control_bgmass, color='red', ax=ax, linestyle=':', hold=True)
-    titles.append(control.title)
-    titles.append("control bg")
-  pl.legend(titles)
+  for field, color, style in zip(fields, colors, styles):
+    plotvar (field, color=color, ax=ax, linestyle=style, hold=True)
+  ax.set_title(title)
+  pl.legend(labels, loc='best')
 
   fig.savefig(outfile)
+
+def totalmass (experiment, control, outdir):
+  from carbontracker import data as ct
+  from os.path import exists
+
+  # CO2 mass
+
+  exper_mass = experiment.get_data('dm', 'totalmass', 'CO2')
+  exper_bgmass = experiment.get_data('dm', 'totalmass', 'CO2B')
+  ct_mass = ct['totalmass']['co2']
+  fields = [exper_mass, exper_bgmass, ct_mass]
+  colors = ['blue', 'blue', 'green']
+  styles = ['-', ':', '-']
+  labels = [experiment.title, 'background', 'CarbonTracker']
+  if control is not None:
+    control_mass = control.get_data('dm', 'totalmass', 'CO2')
+    control_bgmass = control.get_data('dm', 'totalmass', 'CO2B')
+    fields.extend([control_mass, control_bgmass])
+    colors.extend(['red', 'red'])
+    styles.extend(['-', ':'])
+    labels.extend([control.title, 'control bg'])
+
+  outfile = outdir + "/%s_totalmass_CO2.png"%experiment.name
+  if not exists(outfile):
+    doplot (outfile, "Total mass CO2 (Pg C)", fields, colors, styles, labels)
+
 
