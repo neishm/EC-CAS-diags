5dd3a4a8459977166d695e8733069a73f2afa61a
Mike Neish <Michael.Neish@ec.gc.ca>
2014-10-07 14:27:15 +0000

    Add a zonal mean difference diagnostic.
    Note: only works if the experiment and control have identical axes (including
    time axis).

---
+++ movie_zonal_diff.py
@@ -0,0 +1,32 @@
+def movie_zonal_diff (models, fieldname, units, outdir):
+
+  from common import convert
+  from movie_zonal import zonalmean_gph, ZonalMovie
+
+  models = [m for m in models if m is not None]
+  prefix = '_'.join(m.name for m in models) + '_zonal_diff'+fieldname
+  title = 'Zonal mean %s (in %s)'%(fieldname,units)
+  aspect_ratio = 1.0
+  shape = (1,len(models)+1)
+
+  fields = [zonalmean_gph(m,fieldname) for m in models]
+
+  # Unit conversion
+  fields = [convert(f,units) for f in fields]
+
+  subtitles = [m.title for m in models]
+
+  # Plot a difference field as well.
+  if fields[0].axes != fields[1].axes:
+    raise ValueError ("The axes of the fields are not identical")
+  diff = fields[0]-fields[1]
+  diff.name=fieldname+'_diff'
+  # Cache the difference (so we get a global high/low for the colourbar)
+  diff = models[0].cache.write(diff, prefix='zonalmean_gph_diff_'+models[1].name+'_'+fieldname)
+  fields.append(diff)
+  subtitles.append('difference')
+
+  movie = ZonalMovie(fields, title=title, subtitles=subtitles, shape=shape, aspect_ratio=aspect_ratio)
+
+  movie.save (outdir=outdir, prefix=prefix)
+
