19bebc170d6cefb997bfb1c1089473b1f61d445b
Mike Neish <Michael.Neish@ec.gc.ca>
2015-04-30 15:38:10 +0000

    Compile the Fortran modules within setup.py (easier to make a package).

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from distutils.core import setup
+from numpy.distutils.core import setup, Extension
+
+regrid_horz = Extension ('eccas_diags.regrid_horz', sources=['fvdasregridmodule.f90'])
+regrid_vert = Extension ('eccas_diags.regrid_vert', sources=['regrid_vert.f90'])
 setup (
   name="eccas-diags",
   version="0-20150429",
-  packages=["eccas_diags","eccas_diags.interfaces"]
+  packages=["eccas_diags","eccas_diags.interfaces"],
+  ext_modules=[regrid_horz, regrid_vert],
 )
 
