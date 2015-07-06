c2f6981eb2f41f8553fec0e7fcff230a6363d192
Mike Neish <Michael.Neish@ec.gc.ca>
2015-07-06 18:51:59 +0000

    Update all references to the 'eccas' interface to point to 'eccas-dry'.
    Also, update the diags.py script to work with the changes made in this branch.

--- interfaces/eccas_timeseries.py
+++ interfaces/eccas_timeseries.py
@@ -1,9 +1,9 @@
 # Interface for EC-CAS timeseries data.
 # Use the EC-CAS fieldnames / units, and the FSTD timeseries file format.
 
-from . import eccas, fstd_timeseries
+from . import eccas_dry, fstd_timeseries
 
-class ECCAS_Timeseries(fstd_timeseries.FSTD_Timeseries, eccas.ECCAS_Data):
+class ECCAS_Timeseries(fstd_timeseries.FSTD_Timeseries, eccas_dry.ECCAS_Data):
 
   # Method to find all files in the given directory, which can be accessed
   # through this interface.
