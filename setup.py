cc416beb52c7178a5da44621379856a614d2716c
Mike Neish <Michael.Neish@ec.gc.ca>
2015-08-11 18:18:13 +0000

    Update setup.py to see the new diagnostics sub-module.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ regrid_vert = Extension ('eccas_diags.regrid_vert', sources=['regrid_vert.f90'])
 setup (
   name="eccas-diags",
   version="0-20150429",
-  packages=["eccas_diags","eccas_diags.interfaces"],
+  packages=["eccas_diags","eccas_diags.interfaces","eccas_diags.diagnostics"],
   ext_modules=[regrid_horz, regrid_vert],
 )
 
