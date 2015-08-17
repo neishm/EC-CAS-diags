f315846c7af52f556cd0998e15b260dfd10a4824
Mike Neish <Michael.Neish@ec.gc.ca>
2015-08-17 17:50:56 +0000

    Check for cache files in the old location (in tmpdir), so they can be re-used.
    Eventually, this patch should be removed;  it's only useful during the
    transition period to the new cache locations.

--- cache.py
+++ cache.py
@@ -196,6 +196,12 @@ class Cache (object):
 
           filename = self.full_path(prefix+"_split/"+prefix+"_"+datestring+".nc")
           if exists(filename): continue
+
+          # Check old location as well (before split files were moved to a 
+          # subdirectory.
+          filename = self.full_path(prefix+"_"+datestring+".nc")
+          if exists(filename): continue
+
           filename = self.full_path(prefix+"_split/"+prefix+"_"+datestring+".nc", writeable=True)
 
           # Save the data
@@ -212,7 +218,14 @@ class Cache (object):
         pbar.update(100)
 
         # Re-query for the files
-        filenames = [self.full_path(prefix+"_split/"+prefix+"_"+datestring+".nc", existing=True) for datestring in datestrings]
+        filenames = []
+        for datestring in datestrings:
+          # Check old location first
+          filename = self.full_path(prefix+"_"+datestring+".nc")
+          # Otherwise, use new location (separate subdirectory).
+          if not exists(filename):
+            filename = self.full_path(prefix+"_split/"+prefix+"_"+datestring+".nc", existing=True)
+          filenames.append(filename)
 
         # Open the many small files
         var = open_multi(filenames, format=netcdf, pattern="_"+pattern+"\.nc")[var.name]
