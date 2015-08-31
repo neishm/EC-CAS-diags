1654e95461d5d87bc6d05c8998a307c79804e5a3
Mike Neish <Michael.Neish@ec.gc.ca>
2015-08-31 18:58:29 +0000

    New snapshot for 20150831.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ regrid_horz = Extension ('eccas_diags.regrid_horz', sources=['fvdasregridmodule.
 regrid_vert = Extension ('eccas_diags.regrid_vert', sources=['regrid_vert.f90'])
 setup (
   name="eccas-diags",
-  version="0-20150429",
+  version="0-20150831",
   packages=["eccas_diags","eccas_diags.interfaces","eccas_diags.diagnostics"],
   ext_modules=[regrid_horz, regrid_vert],
 )
