8f3f888a2deea36746b9279d25577fd643c4ec1c
Mike Neish <Michael.Neish@ec.gc.ca>
2015-10-21 16:05:56 +0000

    Got all the axes and diagnostic variables "working" for the TM5 interface.
    Can now run the check_interface script on it without crashing.

--- interfaces/tm5.py
+++ interfaces/tm5.py
@@ -13,7 +13,22 @@ class TM5_Data(DataProduct):
   @staticmethod
   def open_file (filename):
     from pygeode.formats import netcdf
-    return netcdf.open(filename)
+    from pygeode.var import Var
+    dataset = netcdf.open(filename)
+    # Put the dA / dB in here.
+    A_interface = dataset.A.get()
+    B_interface = dataset.B.get()
+    dA = (A_interface[1:] - A_interface[:-1])
+    dB = (B_interface[1:] - B_interface[:-1])
+    dA = Var(name='dA', axes=[dataset.LEVEL], values=dA)
+    dB = Var(name='dB', axes=[dataset.LEVEL], values=dB)
+    # Put the A and B coefficients for staggered levels in here.
+    A_t = (A_interface[1:] + A_interface[:-1]) * 0.5
+    B_t = (B_interface[1:] + B_interface[:-1]) * 0.5
+    A_t = Var(name='A_t', axes=[dataset.LEVEL], values=A_t)
+    B_t = Var(name='B_t', axes=[dataset.LEVEL], values=B_t)
+    dataset = dataset + dA + dB + A_t + B_t
+    return dataset
 
 
   # Method to decode an opened dataset (standardize variable names, and add any
@@ -21,32 +36,43 @@ class TM5_Data(DataProduct):
   @classmethod
   def decode (cls, dataset):
     from pygeode.axis import Hybrid
+    from pygeode.timeaxis import StandardTime
     from pygeode.ufunc import exp
     from pygeode.var import Var
     import numpy as np
 
-    A_interface = dataset.A.get()
-    B_interface = dataset.B.get()
-    A = (A_interface[1:] + A_interface[:-1]) * 0.5
-    B = (B_interface[1:] + B_interface[:-1]) * 0.5
-    dA = (A_interface[1:] - A_interface[:-1])
-    dB = (B_interface[1:] - B_interface[:-1])
 
-    #TODO
-
-    # Locate a z-axis
-    dataset = list(dataset)
-    zaxis = None
-    for var in dataset:
-      if var.hasaxis('LEVEL'):
-        zaxis = var.getaxis('LEVEL')
+    # Get some axis info that will be needed for further calculations.
+    A_t = getattr(dataset,'A_t',None)
+    B_t = getattr(dataset,'B_t',None)
+    dA = getattr(dataset,'dA',None)
+    dB = getattr(dataset,'dB',None)
+    zaxis = getattr(dataset,'LEVEL',None)
+    lat = getattr(dataset,'LATITUDE',None)
+    lon = getattr(dataset,'LONGITUDE',None)
+    time = getattr(dataset,'TIME',None)
 
     # Create a proper z-axis
-    if zaxis is not None:
-      zaxis = Hybrid(zaxis.values, A=A, B=B)
-      for i, var in enumerate(dataset):
-        if var.hasaxis('LEVEL'):
-          dataset[i] = var.replace_axes(LEVEL=zaxis)
+    if zaxis is not None and A_t is not None and B_t is not None:
+      zaxis = Hybrid(zaxis.values, A=A_t.get(), B=B_t.get())
+
+    # Create a proper time axis
+    if time is not None:
+      time = StandardTime(month=time.values)
+
+    dataset = list(dataset)
+
+    for i, var in enumerate(dataset):
+      if var.hasaxis('LEVEL'):
+        var = var.replace_axes(LEVEL=zaxis)
+      if var.hasaxis('TIME'):
+        var = var.replace_axes(TIME=time)
+      # Refresh the variables that were already extracted.
+      if var.name == 'A_t': A_t = var
+      if var.name == 'B_t': B_t = var
+      if var.name == 'dA': dA = var
+      if var.name == 'dB': dB = var
+      dataset[i] = var
 
     # Apply fieldname conversions
     dataset = DataProduct.decode.__func__(cls,dataset)
@@ -57,15 +83,11 @@ class TM5_Data(DataProduct):
     # Add pressure info
     if 'surface_pressure' in data and zaxis is not None:
       Ps = data['surface_pressure']
-      A = zaxis.auxasvar('A')
-      B = zaxis.auxasvar('B')
-      P = A + B*Ps
+      P = A_t + B_t*Ps
       P = P.transpose('time','zaxis','lat','lon')
       P.atts['units'] = 'Pa'
       data['air_pressure'] = P
 
-      dA = Var([zaxis], values=dA)
-      dB = Var([zaxis], values=dB)
       dP = dA + dB*Ps
       dP = dP.transpose('time','zaxis','lat','lon')
       dP.atts['units'] = 'Pa'
@@ -73,10 +95,9 @@ class TM5_Data(DataProduct):
 
 
     # Grid cell areas
-    # Pick some arbitrary (but deterministic) variable to get the lat/lon
-    var = (v for v in sorted(data.values()) if v.hasaxis('lat') and v.hasaxis('lon')).next()
     from ..common import get_area
-    data['cell_area'] = get_area(var.lat,var.lon)
+    if lat is not None and lon is not None:
+      data['cell_area'] = get_area(lat,lon)
 
     # General cleanup stuff
 
