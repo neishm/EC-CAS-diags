f75560f10834e8ae8b6b12604624e46dbeccb3bf
Mike Neish <Michael.Neish@ec.gc.ca>
2015-11-10 20:03:26 +0000

    Allow zonal mean to work on whole model dataset.

--- calculations/zonal_stats.py
+++ calculations/zonal_stats.py
@@ -33,4 +33,17 @@ def zonalstd (var):
   return var
 
 
+def all_zonalmean (model):
+  from common import number_of_levels, number_of_timesteps
+  from ..interfaces import DerivedProduct
+  fieldnames = [v.name for d in model.datasets for v in d]
+  fieldnames = sorted(set(fieldnames))
+  outvars = []
+  for fieldname in fieldnames:
+    var = model.find_best(fieldname, maximize=(number_of_levels,number_of_timesteps))
+    if not var.hasaxis('lon'): continue
+    var = zonalmean(var)
+    outvars.append(var)
+  outdata = DerivedProduct(outvars, name=model.name+'_zonalmean', title=model.title, color=model.color, cache=model.cache)
+  return outdata
 
