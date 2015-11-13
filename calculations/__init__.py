71476fd2ab19ba262f992dc7594aee871b8061d7
Mike Neish <Michael.Neish@ec.gc.ca>
2015-11-13 21:15:08 +0000

    Allow the calculation to work on multiple input datasets (e.g. for doing a binary operation like subtraction).
    Also allow the precision of the cached values to be controlled.

--- calculations/__init__.py
+++ calculations/__init__.py
@@ -1,25 +1,23 @@
-# Decorator to complete a unary calculation.
+# Decorator for a simple calculation function.
 #
-# The original function should take a single dataset, and generate the
-# output vars.
-#
-# After applying the decorator, the function will take a data product and
-# return a data product with the calculation applied.
-#
-# If caching is requested, the data will be cached as needed.
-def unary_calc(func=None, cache=False):
+# Allows the function to work on an entire data product.
+# Can also cache the calculation when needed.
+def quick_calc(func=None, cache=False, full_precision=False):
   def wrapper(f):
     from pygeode.dataset import Dataset
     from ..interfaces import DerivedProduct
-    def new_func (m):
+    def new_func (*models):
       out_datasets = []
-      for in_dataset in m.datasets:
+      for in_datasets in zip(*[m.datasets for m in models]):
         try:
-          out_vars = list(f(in_dataset))
+          out_vars = list(f(*in_datasets))
         except KeyError: continue
         if len(out_vars) == 0: continue
+        prefix = models[0].name+'_'+f.__name__
+        if len(models) > 1:
+          prefix += '_' + '_'.join(m.name for m in models[1:])
         if cache is True:
-          out_vars = [m.cache.delayed_write(v, prefix=m.name+'_'+f.__name__+'_'+v.name) for v in out_vars]
+          out_vars = [m.cache.delayed_write(v, prefix=m.name+'_'+f.__name__+'_'+v.name, force_single_precision=(not full_precision)) for v in out_vars]
         out_datasets.append(Dataset(out_vars))
       out_product = DerivedProduct(out_datasets, name=m.name+'_'+f.__name__, title=m.title, color=m.color, cache=m.cache)
       return out_product
--- calculations/zonal_stats.py
+++ calculations/zonal_stats.py
@@ -1,6 +1,6 @@
-from . import unary_calc
+from . import quick_calc
 
-@unary_calc(cache=True)
+@quick_calc(cache=True)
 def zonalmean (dataset):
   from ..common import remove_repeated_longitude
 
@@ -14,8 +14,8 @@ def zonalmean (dataset):
     yield invar.nanmean('lon')
 
 
-@unary_calc(cache=True)
-def zonalrms (dataset):
+@quick_calc(cache=True)
+def zonalstd (dataset):
   from ..common import remove_repeated_longitude
 
   for invar in dataset:
@@ -24,64 +24,7 @@ def zonalrms (dataset):
     # Remove any repeated longtiude (for global data)
     invar = remove_repeated_longitude(invar)
 
-    outvar = (invar**2).nanmean('lon').sqrt()
-    outvar.name = invar.name
-
-    yield outvar
-
-@unary_calc
-def _zonal_filter (dataset):
-  for invar in dataset:
-    if not invar.hasaxis('lon'): continue
-    yield invar
-
-
-def all_zonalmean (model):
-  from common import number_of_levels, number_of_timesteps
-  from ..interfaces import DerivedProduct
-  from pygeode.dataset import Dataset
-  out_datasets = []
-  for in_dataset in model.datasets:
-    out_dataset = []
-    for in_var in in_dataset:
-      if not in_var.hasaxis('lon'): continue
-      out_var = zonalmean(in_var)
-      # Cache the data (delayed)
-      if model.cache is not None:
-        out_var = model.cache.delayed_write(out_var, prefix=model.name+'_zonalmean_'+in_var.name)
-      out_dataset.append(out_var)
-    if len(out_dataset) == 0: continue
-    out_dataset = Dataset(out_dataset)
-    out_datasets.append(out_dataset)
-  out = DerivedProduct(out_datasets, name=model.name+'_zonalmean', title=model.title, color=model.color, cache=model.cache)
-  return out
-
-
-def all_zonalstd (model):
-  from common import number_of_levels, number_of_timesteps
-  from ..interfaces import DerivedProduct
-  from pygeode.dataset import Dataset
-  out_datasets = []
-  for in_dataset in model.datasets:
-    out_dataset = []
-    for in_var in in_dataset:
-      if not in_var.hasaxis('lon'): continue
-      mean_var = zonalmean(in_var)
-      # Cache the mean data
-      if model.cache is not None:
-        # TODO: use the existing all_zonalmean function for this part?
-        mean_var = model.cache.delayed_write(mean_var, prefix=model.name+'_zonalmean_'+in_var.name)
-      # Subtract the mean before computing the standard deviation
-      out_var = zonalstd(in_var-mean_var)
-      out_var.name = in_var.name
-      # Cache the data (delayed)
-      if model.cache is not None:
-        out_var = model.cache.delayed_write(out_var, prefix=model.name+'_zonalstd_'+in_var.name)
-      out_dataset.append(out_var)
-    if len(out_dataset) == 0: continue
-    out_dataset = Dataset(out_dataset)
-    out_datasets.append(out_dataset)
-  out = DerivedProduct(out_datasets, name=model.name+'_zonalstd', title=model.title, color=model.color, cache=model.cache)
-  return out
+    # Do the standard deviation (NOTE: single-pass algorithm, may lose precision)
+    yield invar.nanstdev('lon')
 
 
