5cb889cfbd4991b636df809c6473a555b75af02a
Mike Neish <Michael.Neish@ec.gc.ca>
2015-12-11 01:18:29 +0000

    Be more specific in criteria for CarbonTracker filenames.
    We have a few CT2013B molefractions right in the molefractions/ directory,
    which could confuse the diagnostics.

--- interfaces/carbontracker.py
+++ interfaces/carbontracker.py
@@ -161,10 +161,10 @@ class CT_Data(DataProduct):
   def find_files (dirname):
     from glob import glob
     # CT2010
-    molefractions = glob(dirname+"/molefractions/CT*.molefrac_glb3x2_????-??-??.nc")
+    molefractions = glob(dirname+"/molefractions/CT2010*.molefrac_glb3x2_????-??-??.nc")
     # CT2013B has "total" and "components" products.
-    molefractions += glob(dirname+"/molefractions/co2_total/CT*.molefrac_glb3x2_????-??-??.nc")
-    molefractions += glob(dirname+"/molefractions/co2_components/CT*.molefrac_glb3x2_????-??-??.nc")
+    molefractions += glob(dirname+"/molefractions/co2_total/CT2013B*.molefrac_glb3x2_????-??-??.nc")
+    molefractions += glob(dirname+"/molefractions/co2_components/CT2013B*.molefrac_glb3x2_????-??-??.nc")
     fluxes = glob(dirname+"/fluxes/CT*.flux1x1.????????.nc")
 
     # Blacklist the 2009-08-07 molefractions file, which has bad data at 10:30
