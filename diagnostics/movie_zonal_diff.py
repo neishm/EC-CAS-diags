8fcc60b121e8a8533c357391f2bad2e2d0b9e5a5
Mike Neish <Michael.Neish@canada.ca>
2016-05-11 19:42:48 +0000

    Refactor movie_zonal_diff to use the preprocessing from movie_zonal.

--- diagnostics/movie_zonal_diff.py
+++ diagnostics/movie_zonal_diff.py
@@ -4,59 +4,49 @@ class ZonalMeanDiff(ZonalMean):
   Difference between two zonal mean fields, animated in time.
   """
   def _input_combos (self, inputs):
-    from .movie_zonal import find_applicable_models
     fieldname = self.fieldname
-    models = find_applicable_models(inputs, fieldname, self.zaxis)
-    n = len(models)
+    n = len(inputs)
     for i in range(n):
-      if not models[i].have(fieldname): continue
+      if not inputs[i].have(fieldname): continue
       for j in range(i+1,n):
-        if not models[j].have(fieldname): continue
-        f1 = models[i].find_best(fieldname)
-        f2 = models[j].find_best(fieldname)
+        if not inputs[j].have(fieldname): continue
+        f1 = inputs[i].find_best(fieldname)
+        f2 = inputs[j].find_best(fieldname)
         if f1.lat == f2.lat:
-          yield [models[i],models[j]]
-
-  def do (self, inputs):
-    movie_zonal_diff(inputs, fieldname=self.fieldname, units=self.units, outdir=outdir, zaxis=self.zaxis, typestat=self.typestat, suffix=self.suffix)
-
-if True:
-  def movie_zonal_diff (models, fieldname, units, outdir, zaxis='gph', typestat='mean', suffix=""):
-
-    from ..common import convert, same_times
-    from .movie_zonal import zonalmean_gph, zonalmean_pres, ZonalMovie
-
-    prefix = '_'.join(m.name for m in models) + '_zonal_diff'+typestat+"_"+fieldname+'_on_'+zaxis+suffix
-    title = 'Zonal %s %s (in %s)'%(typestat, fieldname,units)
-    aspect_ratio = 1.0
-    shape = (1,len(models)+1)
-
-    if zaxis == 'gph':
-      fields = [zonalmean_gph(m,fieldname,units,typestat,suffix=suffix) for m in models]
-    else:
-      fields = [zonalmean_pres(m,fieldname,units,suffix=suffix) for m in models]
-
-    # Unit conversion
-    fields = [convert(f,units) for f in fields]
-
-    subtitles = [m.title for m in models]
-
+          yield [inputs[i],inputs[j]]
+  def _transform_inputs (self, inputs):
+    from ..common import same_times
+    from ..interfaces import DerivedProduct
+    inputs = super(ZonalMeanDiff,self)._transform_inputs(inputs)
+    # Plot a difference field as well.
+    fields = [inp.find_best(self.fieldname) for inp in inputs]
     # Use only the common timesteps between the fields
     fields = same_times (*fields)
-
-    # Plot a difference field as well.
-    if fields[0].axes != fields[1].axes:
-      raise ValueError ("The axes of the fields are not identical")
     diff = fields[0]-fields[1]
-    diff.name=fieldname+'_diff'
+    diff.name=self.fieldname+'_diff'
     # Cache the difference (so we get a global high/low for the colourbar)
-    diff = models[0].cache.write(diff, prefix=models[0].name+'_zonal'+typestat+'_gph_diff_'+models[1].name+'_'+fieldname+suffix)
-    fields.append(diff)
-    subtitles.append('difference')
+    diff = inputs[0].cache.write(diff, prefix=inputs[0].name+'_zonal'+self.typestat+'_gph_diff_'+inputs[1].name+'_'+self.fieldname+self.suffix)
+    diff = DerivedProduct(diff, source=inputs[0])
+    diff.name = 'diff'
+    diff.title = 'difference'
+    inputs.append(diff)
+    return inputs
+
+  def do (self, inputs):
+    from .movie import ZonalMovie
+
+    prefix = '_'.join(inp.name for inp in inputs) + '_zonal'+self.typestat+'_'+self.fieldname+'_on_'+self.zaxis+self.suffix
+    title = 'Zonal %s %s (in %s)'%(self.typestat,self.fieldname,self.units)
+    aspect_ratio = 1.0
+    shape = (1,len(inputs))
+
+    subtitles = [inp.title for inp in inputs]
+
+    fields = [inp.datasets[0].vars[0] for inp in inputs]
 
     movie = ZonalMovie(fields, title=title, subtitles=subtitles, shape=shape, aspect_ratio=aspect_ratio)
 
-    movie.save (outdir=outdir, prefix=prefix)
+    movie.save (outdir=self.outdir, prefix=prefix)
 
 from . import table
 table['zonal-mean-diff'] = ZonalMeanDiff
