3300635f2152b3347f7dec673ac9a62e9232fdd5
Mike Neish <Michael.Neish@canada.ca>
2016-05-20 16:24:00 +0000

    Move the totalmass and totalflux calculations to a _transform_inputs() step.
    This way, the calculations can be re-used by other diagnostics that inherit
    from this one.
    
    Also, instead of have a separate check for pre-conditions, just try to do the
    calculation and see what happens.  If it fails, then that dataset won't be
    considered for the totalmass plots.

--- diagnostics/totalmass.py
+++ diagnostics/totalmass.py
@@ -1,18 +1,4 @@
 
-if True:
-
-
-  def find_applicable_models (inputs, fieldname):
-    from ..common import have_gridded_3d_data
-    models = []
-    for x in inputs:
-      if any ((fieldname in d and have_gridded_3d_data(d)) or fieldname+'_flux' in d for d in x.datasets):
-        models.append(x)
-      elif fieldname == 'air' and any('dp' in d for d in x.datasets):
-        models.append(x)
-    return models
-
-
 from . import TimeVaryingDiagnostic, ImageDiagnostic
 class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
   """
@@ -22,27 +8,76 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
   def __init__ (self, **kwargs):
     super(Totalmass,self).__init__(**kwargs)
     self.require_fieldname = False # Will provide our own checks below.
-  def _check_dataset(self,dataset):
-    from ..common import can_convert
-    if not super(Totalmass,self)._check_dataset(dataset):
-      return False
-    if self.fieldname in dataset and 'dp' in dataset and 'cell_area' in dataset:
-      return True
-    fluxname = self.fieldname+'_flux'
-    if fluxname in dataset:
-      if can_convert(dataset[fluxname],'g s-1'):
-        return True
-      if can_convert(dataset[fluxname],'g m-2 s-1') and 'cell_area' in dataset:
-        return True
-    return False
 
   def _select_inputs (self, inputs):
     inputs = super(Totalmass,self)._select_inputs(inputs)
-    return find_applicable_models(inputs, self.fieldname)
+    selected = []
+    for inp in inputs:
+      try:
+        totalmass = self._compute_totalmass(inp,cache=False)
+        selected.append(inp)
+        continue
+      except KeyError: pass
+      try:
+        totalflux = self._compute_totalflux(inp,cache=False)
+        selected.append(inp)
+        continue
+      except KeyError: pass
+    return selected
 
+  # Compute the mass for each input.
+  def _transform_inputs (self, inputs):
+    from ..common import convert
+    from ..interfaces import DerivedProduct
+    from pygeode import timeutils
+    from pygeode.var import Var
+    inputs = super(Totalmass,self)._transform_inputs(inputs)
+
+    computed = []
+    last_totalmass = None
+    for inp in inputs:
+      try:
+        totalmass = self._compute_totalmass (inp)
+        totalmass = convert(totalmass,self.units)
+        computed.append(DerivedProduct(totalmass, source=inp))
+        last_totalmass = totalmass
+      except KeyError: pass
+      try:
+        totalflux = self._compute_totalflux (inp)
+        totalflux = convert(totalflux, self.units+' s-1')
+        data = totalflux.get()
+        # Get time interval
+        time = totalflux.time
+        dt = timeutils.delta(time, units='seconds')
+        # Integrate over the flux period
+        data = data * dt
+        # Running sum
+        data = data.cumsum()
+        # Initial time is *after* the first sum
+        assert time.units == 'days'
+        time = time.__class__(values=time.values+dt/86400., units='days', startdate=time.startdate)
+        # Re-wrap as a PyGeode var
+        totalmass = Var([time], values=data, name=totalflux.name)
+        # Update the flux units to reflect the time integration
+        totalmass.atts['units'] = self.units
+        # Identify the tracer specie (so the unit converter knows what to use for
+        if 'specie' in totalflux.atts:
+          totalmass.atts['specie'] = totalflux.atts['specie']
+
+        # Find the closest "start" time in the flux data that aligns with the last model data
+        if last_totalmass is not None:
+          tx = float(min(set(time.values) & set(totalmass.time.values)))
+          # Offset the flux mass
+          totalmass -= float(totalmass(time=tx).get().squeeze())
+          totalmass += float(last_totalmass(time=tx).get().squeeze())
+        totalmass = DerivedProduct(totalmass, source=inp)
+        totalmass.title += ' (integrated flux)'
+        computed.append(totalmass)
+      except KeyError: pass
+    return computed
 
   # Total mass (Pg)
-  def _compute_totalmass (self, model):
+  def _compute_totalmass (self, model, cache=True):
     from ..common import can_convert, convert, find_and_convert, grav as g, number_of_levels, number_of_timesteps, remove_repeated_longitude
     fieldname = self.fieldname
     suffix = self.suffix
@@ -76,8 +111,8 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
     # shortcut
     elif fieldname == 'air':
        from warnings import warn
-       warn ("No 'dp' data found in '%s'.  Approximating total air mass from surface pressure"%model.name)
        p0, area = model.find_best(['surface_pressure','cell_area'], maximize=number_of_timesteps)
+       warn ("No 'dp' data found in '%s'.  Approximating total air mass from surface pressure"%model.name)
        p0 = convert(p0,'Pa')
        tc = p0 / g
     else:
@@ -97,10 +132,12 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
       data.atts['specie'] = specie
 
     # Cache the data
-    return model.cache.write(data,prefix=model.name+"_totalmass_"+fieldname+suffix, force_single_precision=False)
+    if cache:
+      data =  model.cache.write(data,prefix=model.name+"_totalmass_"+fieldname+suffix, force_single_precision=False)
+    return data
 
   # Integrated flux (moles per second)
-  def _compute_totalflux (self, model):
+  def _compute_totalflux (self, model, cache=True):
     from ..common import convert, number_of_timesteps, remove_repeated_longitude
 
     fieldname = self.fieldname
@@ -128,7 +165,9 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
     data.name = fieldname
 
     # Cache the data
-    return model.cache.write(data,prefix=model.name+"_totalflux_"+fieldname+suffix, force_single_precision=False)
+    if cache:
+      data =  model.cache.write(data,prefix=model.name+"_totalflux_"+fieldname+suffix, force_single_precision=False)
+    return data
 
   @staticmethod
   def _doplot (outfile, title, fields, colours, styles, labels):
@@ -146,9 +185,6 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
 
   def do (self, inputs):
     from os.path import exists
-    from pygeode.var import Var
-    from ..common import convert
-    from pygeode import timeutils
 
     fieldname = self.fieldname
     units = self.units
@@ -157,20 +193,11 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
     suffix = self.suffix
 
     # Find common time period
-    #TODO:  a method to query the time axis from the model without needing to grab an actual diagnostic field.
     t0 = []
     t1 = []
     for inp in inputs:
-      try:
-        t0.append(self._compute_totalmass(inp).time.values[0])
-        t1.append(self._compute_totalmass(inp).time.values[-1])
-      except Exception: pass
-      try:
-        t0.append(self._compute_totalflux(inp).time.values[0])
-        t1.append(self._compute_totalflux(inp).time.values[-1])
-      except Exception: pass
-    if len(t0) == 0 or len(t1) == 0:
-      raise ValueError("Unable to find any '%s' data in %s."%(fieldname,[inp.name for inp in inputs]))
+      t0.append(inp.datasets[0].time.values[0])
+      t1.append(inp.datasets[0].time.values[-1])
     t0 = max(t0)
     t1 = min(t1)
 
@@ -181,64 +208,13 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
     labels = []
 
     for i,model in enumerate(inputs):
-      if model is None: continue
 
-      # Check for 3D fields, compute total mass.
-      try:
-        # Total mass
-        # Possibly change plot units (e.g. Pg CO2 -> Pg C)
-        mass = self._compute_totalmass(model)
-        mass = convert(mass, units)
-        mass = mass(time=(t0,t1))   # Limit time period to plot
-        fields.append(mass)
-        colours.append(model.color)
-        styles.append(model.linestyle)
-        labels.append(model.title)
-      except KeyError: pass  # No 3D field available.
-
-      # Total flux, integrated in time
-      try:
-        totalflux = self._compute_totalflux(model)
-        flux_units = totalflux.atts['units']
-        flux_specie = totalflux.atts['specie']
-        time = totalflux.time
-
-        # Find the closest "start" time in the flux data that aligns with the last model data
-        mass = fields[-1]
-        try:
-          tx = float(min(set(time.values) & set(mass.time.values)))
-        except ValueError:   # No common timesteps between fluxes and model data?
-          tx = min(tx for tx in time.values if tx > t0)
-
-        totalflux = totalflux.get()
-        # Get time interval
-        dt = timeutils.delta(time, units='seconds')
-        # Integrate over the flux period
-        totalflux = totalflux * dt
-        # Running sum
-        totalflux = totalflux.cumsum()
-        # Initial time is *after* the first sum
-        assert time.units == 'days'
-        time = time.__class__(values=time.values+dt/86400., units='days', startdate=time.startdate)
-        # Re-wrap as a PyGeode var
-        totalflux = Var([time], values=totalflux, name=fieldname)
-        # Update the flux units to reflect the time integration
-        totalflux.atts['units'] = flux_units + ' s'
-        # Identify the tracer specie (so the unit converter knows what to use for
-        # molar mass, etc.)
-        totalflux.atts['specie'] = flux_specie
-        # Convert from moles to Pg
-        totalflux = convert(totalflux, units)
-        # Offset the flux mass
-        totalflux -= float(totalflux(time=tx).get().squeeze())
-        totalflux += float(mass(time=tx).get().squeeze())
-        # Limit the time period to plot
-        totalflux = totalflux(time=(t0,t1))
-        fields.append(totalflux)
-        colours.append(model.color)
-        styles.append(model.linestyle)
-        labels.append('integrated flux')
-      except (KeyError, IndexError): pass  # No flux and/or mass field available
+      mass = model.find_best(self.fieldname)
+      mass = mass(time=(t0,t1))   # Limit time period to plot
+      fields.append(mass)
+      colours.append(model.color)
+      styles.append(model.linestyle)
+      labels.append(model.title)
 
     outfile = outdir + "/%s_totalmass_%s%s.%s"%('_'.join(inp.name for inp in inputs),fieldname,suffix,format)
     if not exists(outfile):
