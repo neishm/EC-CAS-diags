neishm/EC-CAS-diags

commit 807c9d47f4f682df2174e987605aa9e0b89fff08

Use the more general find_and_convert() command for encoding data in interfaces.

interfaces/__init__.py
@@ -180,36 +180,23 @@ class DataProduct (DataInterface):
   @classmethod
   def encode (cls, dataset):
     from pygeode.dataset import asdataset
-    from ..common import convert
+    from ..common import find_and_convert
     from warnings import warn
 
-    # Convert to a dictionary (for referencing by variable name)
-    data = dict((var.name,var) for var in dataset)
+    # Warn about fields that aren't on the list (and won't be encoded)
+    for var in dataset:
+      if all(var.name != standard_name for local_name, standard_name, units in cls.field_list):
+        warn ("Not encoding unrecognized field '%s'"%var.name)
 
-    # Convert the field names and units
+    # Convert to the expected units and rename
+    data = []
     for local_name, standard_name, units in cls.field_list:
-      if standard_name in data:
-        var = data.pop(standard_name)
+      if standard_name in dataset:
         try:
-          var = convert(var, units)
+          var = find_and_convert(dataset, standard_name, units)
+          data.append(var.rename(local_name))
         except ValueError as e:
           warn ("Unable to encode '%s' to '%s': %s"%(standard_name, local_name, e))
-          continue
-        data[local_name] = var
-
-    # Check for any stragglers, remove them
-    for varname in data.keys():
-      if all(varname != name for name, n, u in cls.field_list):
-        warn ("Dropping unrecognized field '%s'"%varname)
-        data.pop(varname)
-
-    # General cleanup stuff
-
-    # Make sure the variables have the appropriate names
-    for name, var in data.iteritems():  var.name = name
-
-    # Convert to a list
-    data = list(data.values())
 
     return data
 

commit 6188c28e5f0ec95359cb1f837e5d903eaeb28f81

Fix bug introduced by 807c9d47f4f682df2174e987605aa9e0b89fff08
Was trying to check if a field name was in a dataset, but was looking at a list
in some cases (check would fail).

interfaces/__init__.py
@@ -183,6 +183,8 @@ class DataProduct (DataInterface):
     from ..common import find_and_convert
     from warnings import warn
 
+    dataset = asdataset(dataset)
+
     # Warn about fields that aren't on the list (and won't be encoded)
     for var in dataset:
       if all(var.name != standard_name for local_name, standard_name, units in cls.field_list):