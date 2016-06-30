b4e9ce08c62603231a1733be6991a4080005751a
Mike Neish <Michael.Neish@canada.ca>
2016-06-30 22:53:54 +0000

    Need to allow extra arguments through the quickcalc wrapper.

--- calculations/__init__.py
+++ calculations/__init__.py
@@ -6,13 +6,13 @@ def quick_calc(f):
   from pygeode.dataset import Dataset
   from ..interfaces import DerivedProduct
   from pygeode.var import Var
-  def new_func (model):
+  def new_func (model,*args,**kwargs):
     # Special case: single variable (no data product metadata to deal with)
-    if isinstance(model,Var): return list(f(Dataset([model])))[0]
+    if isinstance(model,Var): return list(f(Dataset([model]),*args,**kwargs))[0]
     out_datasets = []
     for in_dataset in model.datasets:
       try:
-        out_vars = list(f(in_dataset))
+        out_vars = list(f(in_dataset,*args,**kwargs))
       except KeyError: continue
       if len(out_vars) == 0: continue
       out_datasets.append(Dataset(out_vars))
