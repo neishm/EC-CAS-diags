e84d19db05de27b655772301e65cb2db8ae3c737
Mike Neish <Michael.Neish@canada.ca>
2016-07-28 19:18:35 +0000

    Fix bug in totalmass - wasn't finding the correct initial point between mass and integrated flux curves.

--- diagnostics/totalmass.py
+++ diagnostics/totalmass.py
@@ -66,7 +66,7 @@ class Totalmass(TimeVaryingDiagnostic,ImageDiagnostic):
 
         # Find the closest "start" time in the flux data that aligns with the last model data
         if last_totalmass is not None:
-          tx = float(min(set(time.values) & set(totalmass.time.values)))
+          tx = float(min(set(time.values) & set(last_totalmass.time.values)))
           # Offset the flux mass
           totalmass -= float(totalmass(time=tx).get().squeeze())
           totalmass += float(last_totalmass(time=tx).get().squeeze())
