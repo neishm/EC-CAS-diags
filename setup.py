d474f619de1575e5fcb9fd5fe52d5cd563dc2e44
Mike Neish <Michael.Neish@canada.ca>
2016-09-27 19:13:00 +0000

    Install the command-line scripts from setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ regrid_vert = Extension ('eccas_diags.regrid_vert', sources=['eccas_diags/regrid
 setup (
   name="eccas-diags",
   version="0-20150831",
+  scripts=["eccas-diags","eccas-met-diags","eccas-regrid"],
   packages=["eccas_diags","eccas_diags.interfaces","eccas_diags.diagnostics","eccas_diags.calculations"],
   ext_modules=[regrid_horz, regrid_vert],
 )
