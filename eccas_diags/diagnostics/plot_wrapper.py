024879e98c1c2a5e4a4afd473e35befb4f0821ac
Mike Neish <Michael.Neish@canada.ca>
2016-10-18 21:13:47 +0000

    Add LGPL3 license to the code.

--- eccas_diags/diagnostics/plot_wrapper.py
+++ eccas_diags/diagnostics/plot_wrapper.py
@@ -1,3 +1,24 @@
+###############################################################################
+# Copyright 2016 - Climate Research Division
+#                  Environment and Climate Change Canada
+#
+# This file is part of the "EC-CAS diags" package.
+#
+# "EC-CAS diags" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "EC-CAS diags" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with "EC-CAS diags".  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+
 # Wrapper for matplotlib.pyplot
 # Allows plots to be constructed in a slightly more object-oriented way.
 # Also allows plot objects to be saved to / loaded from files, something which
