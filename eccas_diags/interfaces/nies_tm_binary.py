ee4675be072c9cedebacb86bd6a16ef4615c73e1
Mike Neish <Michael.Neish@canada.ca>
2017-09-14 15:02:08 +0000

    Modify NIES interface so it only loads data as it is needed.

--- eccas_diags/interfaces/nies_tm_binary.py
+++ eccas_diags/interfaces/nies_tm_binary.py
@@ -20,41 +20,17 @@
 
 # Interface for reading NEIS-TM CH4 model output.
 
-from . import DataProduct
-class NIES_TM(DataProduct):
-  """
-  Methane molefractions and fluxes from NIES-TM model output.
-  """
-
-  # Interfaces for the vertical levels.
-  sigma_interface = [1.,.990,.960,.930,.900,.870,.845,.825,.795,.700,.615, 
-                    .540,.470,.410,.355,.310,.265,.225,.195,.166,.141,.121,
-                    .104,.088,.076,.065,.055,.040,.030,.020,.010,.003,.001]
-
-  # List of all possible fields we expect from the data
-  # (original_name, standard_name, units)
-  field_list = (
-    ('ch4vm', 'CH4', 'mol mol(semidry_air)-1'),
-    ('PS', 'surface_pressure', 'Pa'),
-    ('cag', 'CH4_agwaste_flux', 'g(C) m-2 day-1'),
-    ('cbb', 'CH4_bioburn_flux', 'g(C) m-2 day-1'),
-    ('cff', 'CH4_fossil_flux', 'g(C) m-2 day-1'),
-    ('cna', 'CH4_natural_flux', 'g(C) m-2 day-1'),
-    ('coc', 'CH4_ocean_flux', 'g(C) m-2 day-1'),
-  )
-
-
-  # Method to open a single file
-  @staticmethod
-  def open_file (filename):
+from pygeode.var import Var
+class NIES_TM_Var(Var):
+  def __init__ (self, filename):
     import numpy as np
+    from datetime import datetime, timedelta
     from os.path import dirname, basename, join
     import re
-    from datetime import datetime, timedelta
     from pygeode.var import Var
     from pygeode.axis import Lat, Lon, ZAxis
     from pygeode.timeaxis import StandardTime
-    from pygeode.dataset import Dataset
+
     # Parse filename to get fieldname, etc.
     m = re.match(r'(D\d+\.|)(?P<name>\w+?)(mn(?P<month>\d+)|)\.bin', basename(filename))
     if m is None:
@@ -69,6 +45,8 @@ class NIES_TM(DataProduct):
         if len(line) != 2: continue
         key, value = line
         control[key] = value
+
+    # Get axes
     nlon, lontype, lon0, dlon = control['XDEF'].split()
     nlon = int(nlon)
     assert lontype == 'LINEAR'
@@ -113,11 +91,63 @@ class NIES_TM(DataProduct):
       time = StandardTime(year=[t0.year], month=[int(m.group('month'))], units='days', startdate=refdate)
     else:
       raise ValueError("Unrecognized time increment '%s'"%dt)
-    with open(filename,'rb') as f:
-      values = np.fromfile(file=f,dtype='>f4',count=-1).reshape(nt,nlev,nlat,nlon)
-    var = Var(axes=(time,lev,lat,lon), name=name, values=values)
+
+    Var.__init__(self, axes=(time,lev,lat,lon), name=name, dtype='float32')
+    self._filename = filename
+
+  def getvalues (self, start, count):
+    import numpy as np
+    # Get offset into the file (limit to the selected times).
+    nt, nlev, nlat, nlon = self.shape
+    offset = start[0]*nlev*nlat*nlon*4
+    # Figure out how much data to load for the specified times.
+    length = count[0]*nlev*nlat*nlon
+    # Special case: only one timestep selected, so can subset directly on levels.
+    if count[0] == 1:
+      length = count[1]*nlat*nlon
+    # Loadthe data
+    with open(self._filename,'rb') as f:
+      f.seek(offset)
+      values = np.fromfile(file=f,dtype='>f4',count=length).reshape(count[0],-1,nlat,nlon)
+    # Now, subset the other axes.
+    values = values[:,start[1]:start[1]+count[1],start[2]:start[2]+count[2],start[3]+start[3]:count[3]]
+    return values
+
+from . import DataProduct
+class NIES_TM(DataProduct):
+  """
+  Methane molefractions and fluxes from NIES-TM model output.
+  """
+
+  # Interfaces for the vertical levels.
+  sigma_interface = [1.,.990,.960,.930,.900,.870,.845,.825,.795,.700,.615, 
+                    .540,.470,.410,.355,.310,.265,.225,.195,.166,.141,.121,
+                    .104,.088,.076,.065,.055,.040,.030,.020,.010,.003,.001]
+
+  # List of all possible fields we expect from the data
+  # (original_name, standard_name, units)
+  field_list = (
+    ('ch4vm', 'CH4', 'mol mol(semidry_air)-1'),
+    ('PS', 'surface_pressure', 'Pa'),
+    ('cag', 'CH4_agwaste_flux', 'g(C) m-2 day-1'),
+    ('cbb', 'CH4_bioburn_flux', 'g(C) m-2 day-1'),
+    ('cff', 'CH4_fossil_flux', 'g(C) m-2 day-1'),
+    ('cna', 'CH4_natural_flux', 'g(C) m-2 day-1'),
+    ('coc', 'CH4_ocean_flux', 'g(C) m-2 day-1'),
+  )
+
+
+  # Method to open a single file
+  @staticmethod
+  def open_file (filename):
+    from pygeode.dataset import Dataset
+
+    # Get interface to the data.
+    var = NIES_TM_Var(filename)
+
     # Ugly hack to ensure PS & CH4 have the same times.
     var = var(year=(2014,2017))
+
     # Remove degenerate vertical axis for flux & PS data.
     if len(var.lev) == 1:
       var = var.squeeze('lev')
